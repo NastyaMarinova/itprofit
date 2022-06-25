4b1c29682e44780fee66a04fdea2517840178a15
Ana_Stasya <anastasiya-b7@mail.ru>
2022-06-25 10:39:28 +0300

    Update.Directive part II

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { ZoomDirective } from './user-profile/zoom.directive';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserVisibleDirective } from './user-profile/user-visible.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    UserVisibleDirective,
+    ZoomDirective,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-profile/user-visible.directive.ts
@@ -0,0 +1,38 @@
+import { Directive, ViewContainerRef, OnInit, EmbeddedViewRef, TemplateRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appUserVisible]'
+})
+export class UserVisibleDirective implements OnInit{
+
+ private embeddedViewRef: EmbeddedViewRef <any>;
+
+  @Input() 
+  public set appUserVisible (value: boolean){
+    this._blockIsVisible = value;
+
+    if (this._blockIsVisible) {
+      this.embeddedViewRef =this.viewContainer.createEmbeddedView(this.templateRef);
+
+    } else if (!this._blockIsVisible) {
+       
+      this.viewContainer.remove(this.viewContainer.indexOf (this.embeddedViewRef));
+    
+    }
+  }
+
+  public get blockIsVisible(): boolean{
+    return this._blockIsVisible;
+  }
+  private _blockIsVisible: boolean = false;
+
+
+  constructor(
+      private viewContainer: ViewContainerRef,
+      private templateRef: TemplateRef<any>
+      ) { }
+ngOnInit(): void {
+   
+}
+
+  }
